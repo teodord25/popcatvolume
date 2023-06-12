f55b873008eb1a6cd038d0582a4014442b83d1e0
teodord25 <djuric.teodor25@gmail.com>
2023-06-12 17:56:05 +0200

    add check

--- main.sh
+++ main.sh
@@ -8,5 +8,10 @@ elif [[ $1 == vol ]]; then
     pactl list sinks | grep '^[[:space:]]Volume:' | grep -o '[0-9]*%' | sed -n '1p'
 
 elif [[ $1 == show ]]; then
+    if [[ -z $2 ]]; then
+        echo "missing filename"
+        exit 1
+    fi
+
     kitty +kitten icat images/$2.png
 fi
