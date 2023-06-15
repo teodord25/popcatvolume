b51b3ab39f5d8a233d7e25a91bc791e2d66a76fa
teodord25 <djuric.teodor25@gmail.com>
2023-06-15 10:34:19 +0200

    button

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::path::Path;
 use gtk::prelude::*;
-use gtk::{ Window, WindowType };
+use gtk::{ Window, WindowType, Image, Orientation, Button };
 
 fn main() {
     gtk::init().expect("Failed to initialize GTK");
@@ -7,6 +8,21 @@ fn main() {
     let window = Window::new(WindowType::Toplevel);
     window.set_title("Hello GTK!");
     window.set_default_size(200, 200);
+
+    let vbox = gtk::Box::new(Orientation::Vertical, 0);
+    window.add(&vbox);
+
+    let image = Image::from_file("images/0.png");
+    vbox.pack_start(&image, true, true, 0);
+
+    let button = Button::with_label("Click me!");
+    vbox.pack_start(&button, false, false, 0);
+
+    let image_clone = image.clone();
+    button.connect_clicked(move |_| {
+        image_clone.set_from_file(Some(Path::new("images/1.png")));
+    });
+
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
