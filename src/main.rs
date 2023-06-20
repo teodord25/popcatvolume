87e7210aa320517bfce6e6b6943fad8e8236aabc
teodord25 <djuric.teodor25@gmail.com>
2023-06-20 16:07:31 +0200

    change kinda works?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::time::Duration;
+use std::env;
 use gtk::prelude::*;
 use gtk::{Window, WindowType, Image, Orientation};
 use gtk::glib::Continue;
@@ -28,8 +29,7 @@ fn main() {
     let image = Image::from_file("images/0.png");
     vbox.pack_start(&image, true, true, 0);
 
-    // Set up a timeout function to periodically poll the volume.
-    // This function is called every 1000 milliseconds (1 second).
+    let window_clone = window.clone();
     gtk::glib::timeout_add_local(Duration::from_millis(1000), {
         let image = image.clone();
         move || {
@@ -50,12 +50,30 @@ fn main() {
 
             let volume = output_str.parse::<i32>().unwrap();
 
-            if volume == 0       { image.set_from_file(Some("images/0.png")); }
-            else if volume < 25  { image.set_from_file(Some("images/1.png")); }
-            else if volume < 50  { image.set_from_file(Some("images/2.png")); }
-            else if volume < 75  { image.set_from_file(Some("images/3.png")); }
-            else if volume < 100 { image.set_from_file(Some("images/4.png")); }
-            else                 { image.set_from_file(Some("images/5.png")); }
+            let goober: i32 = env::var("GOOBER").unwrap_or_else(|_| "0".to_string()).parse().unwrap_or(0);
+
+             // If the volume has changed
+            if volume != goober {
+                // Set the new volume to GOOBER environment variable
+                env::set_var("GOOBER", volume.to_string());
+
+                if volume == 0       { image.set_from_file(Some("images/0.png")); }
+                else if volume < 25  { image.set_from_file(Some("images/1.png")); }
+                else if volume < 50  { image.set_from_file(Some("images/2.png")); }
+                else if volume < 75  { image.set_from_file(Some("images/3.png")); }
+                else if volume < 100 { image.set_from_file(Some("images/4.png")); }
+                else                 { image.set_from_file(Some("images/5.png")); }
+
+                window_clone.show_now();
+
+                // Setup another timeout to hide the window after 2 seconds
+                let window_clone_2 = window_clone.clone();
+                gtk::glib::timeout_add_local(Duration::from_secs(2), move || {
+                    window_clone_2.hide();
+                    Continue(false)
+                });
+            }
+
 
             // We return `Continue(true)` so that the timeout function is called again after the timeout period.
             // If we returned `Continue(false)`, the function would not be called again.
